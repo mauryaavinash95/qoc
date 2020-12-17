256e2406d14f84ed548ecc10d11dc81ea51d902d
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-12-17 16:28:41 -0600

    Use jax.numpy instead of autograd.numpy

--- qoc/standard/functions/convenience.py
+++ qoc/standard/functions/convenience.py
@@ -6,10 +6,7 @@ i.e. those that don't begin with '_', are autograd compatible.
 
 from functools import reduce
 
-from autograd.extend import defvjp, primitive
-import autograd.numpy as anp
-import numpy as np
-import scipy.linalg as la
+import jax.numpy as jnp
 
 ### COMPUTATIONS ###
 
@@ -24,7 +21,7 @@ def commutator(a, b):
     Returns:
     _commutator :: numpy.ndarray - the commutator of a and b
     """
-    commutator_ = anp.matmul(a, b) - anp.matmul(b, a)
+    commutator_ = jnp.matmul(a, b) - jnp.matmul(b, a)
 
     return commutator_
 
@@ -41,7 +38,7 @@ def conjugate_transpose(matrix):
     _conjugate_tranpose :: numpy.ndarray the conjugate transpose
         of matrix
     """
-    conjugate_transpose_ = anp.conjugate(anp.swapaxes(matrix, -1, -2))
+    conjugate_transpose_ = jnp.conjugate(jnp.swapaxes(matrix, -1, -2))
     
     return conjugate_transpose_
 
@@ -55,7 +52,7 @@ def krons(*matrices):
     operation_policy :: qoc.OperationPolicy - what data type is
         used to perform the operation and with which method
     """
-    krons_ = reduce(anp.kron, matrices)
+    krons_ = reduce(jnp.kron, matrices)
 
     return krons_
 
@@ -69,7 +66,7 @@ def matmuls(*matrices):
     operation_policy :: qoc.OperationPolicy - what data type is
         used to perform the operation and with which method
     """
-    matmuls_ = reduce(anp.matmul, matrices)
+    matmuls_ = reduce(jnp.matmul, matrices)
 
     return matmuls_
 
@@ -84,9 +81,9 @@ def rms_norm(array):
     Returns:
     norm :: float - The rms norm of the array.
     """
-    square_norm = anp.sum(array * anp.conjugate(array))
-    size = anp.prod(anp.shape(array))
-    rms_norm_ = anp.sqrt(square_norm / size)
+    square_norm = jnp.sum(array * jnp.conjugate(array))
+    size = jnp.prod(jnp.shape(array))
+    rms_norm_ = jnp.sqrt(square_norm / size)
     
     return rms_norm_
 
@@ -96,9 +93,9 @@ def rms_norm(array):
 # A row vector is np.array([[0, 1, 2]])
 # A column vector is np.array([[0], [1], [2]])
 column_vector_list_to_matrix = (lambda column_vector_list:
-                                anp.hstack(column_vector_list))
+                                jnp.hstack(column_vector_list))
 
 
 matrix_to_column_vector_list = (lambda matrix:
-                                anp.stack([anp.vstack(matrix[:, i])
+                                jnp.stack([jnp.vstack(matrix[:, i])
                                            for i in range(matrix.shape[1])]))
