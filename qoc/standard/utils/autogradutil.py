70518d9a9f6677432af83baca728a021a244654c
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-12-17 16:43:19 -0600

    Use jax.value_and_grad

--- qoc/standard/utils/autogradutil.py
+++ qoc/standard/utils/autogradutil.py
@@ -2,12 +2,8 @@
 autogutil.py - This module provides utilities for interfacing with autograd.
 """
 
-from autograd.core import make_vjp as _make_vjp
-from autograd.extend import vspace
-from autograd.wrap_util import unary_to_nary
-import numpy as np
+import jax
 
-@unary_to_nary
 def ans_jacobian(function, argnum):
     """
     Get the value and the jacobian of a function.
@@ -23,9 +19,5 @@ def ans_jacobian(function, argnum):
         that returns the value of `function` and the jacobian
         of `function` evaluated at a given argument of `function`
     """
-    vjp, ans = _make_vjp(function, argnum)
-    ans_vspace = vspace(ans)
-    jacobian_shape = ans_vspace.shape + vspace(argnum).shape
-    grads = list(map(vjp, ans_vspace.standard_basis()))
-    jacobian = np.reshape(np.stack(grads), jacobian_shape)
-    return ans, jacobian
+    value_and_grad = jax.value_and_grad(function, argnum)
+    return value_and_grad
