2d2f1b9d2683003e73055f2fd3fe60b0e04fc299
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2022-02-20 15:40:22 -0600

    Remove expm because we use it from JAX instead

--- qoc/standard/functions/__init__.py
+++ qoc/standard/functions/__init__.py
@@ -9,11 +9,9 @@ from qoc.standard.functions.convenience import (commutator,
                                                 rms_norm,
                                                 column_vector_list_to_matrix,
                                                 matrix_to_column_vector_list,)
-from qoc.standard.functions.expm import expm
 
 __all__ = [
     "commutator", "conjugate_transpose", "krons", "matmuls",
     "rms_norm",
-    "column_vector_list_to_matrix", "matrix_to_column_vector_list",
-    "expm",
+    "column_vector_list_to_matrix", "matrix_to_column_vector_list"
 ]
