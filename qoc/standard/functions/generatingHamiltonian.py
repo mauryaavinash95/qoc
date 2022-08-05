958e8436a8e4d0038d0d37ac1316eda2fe9a5a94
Lamb-b <57016503+Lamb-b@users.noreply.github.com>
2022-08-05 16:54:57 -0500

    update jnp array with x.at[idx].set(y)

--- qoc/standard/functions/generatingHamiltonian.py
+++ qoc/standard/functions/generatingHamiltonian.py
@@ -29,14 +29,14 @@ def corner_terms(length, width, sigma = jnp.array(qt.operators.sigmaz().data.toa
     for plaquette_idx in range(0, plaquette_count):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length - 1 ) )
         first_qubit_idx = (length + (length - 1) ) * plaquette_row + plaquette_remainder
-        flags[corner_idx, first_qubit_idx] = True
-        flags[corner_idx, first_qubit_idx + (length - 1)] = True
-        flags[corner_idx + 1, first_qubit_idx] = True
-        flags[corner_idx + 1, first_qubit_idx + length] = True
-        flags[corner_idx + 2, first_qubit_idx + (length - 1)] = True
-        flags[corner_idx + 2, first_qubit_idx + length + (length - 1)] = True
-        flags[corner_idx + 3, first_qubit_idx + length] = True
-        flags[corner_idx + 3, first_qubit_idx + length + (length - 1)] = True
+        flags = flags.at[corner_idx, first_qubit_idx].set(True)
+        flags = flags.at[corner_idx, first_qubit_idx + (length - 1)].set(True)
+        flags = flags.at[corner_idx + 1, first_qubit_idx].set(True)
+        flags = flags.at[corner_idx + 1, first_qubit_idx + length].set(True)
+        flags = flags.at[corner_idx + 2, first_qubit_idx + (length - 1)].set(True)
+        flags = flags.at[corner_idx + 2, first_qubit_idx + length + (length - 1)].set(True)
+        flags = flags.at[corner_idx + 3, first_qubit_idx + length].set(True)
+        flags = flags.at[corner_idx + 3, first_qubit_idx + length + (length - 1)].set(True)
         corner_idx += 4
     
     assert(corner_idx == corner_count)
@@ -68,10 +68,10 @@ def plaquette_terms(length, width,
     for plaquette_idx in range(0, plaquette_count):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length - 1 ) )
         first_qubit_idx = (length + (length - 1) ) * plaquette_row + plaquette_remainder
-        flags[plaquette_idx, first_qubit_idx] = 1
-        flags[plaquette_idx, first_qubit_idx + (length - 1)] = -1
-        flags[plaquette_idx, first_qubit_idx + length] = -1
-        flags[plaquette_idx, first_qubit_idx + length + (length - 1)] = 1
+        flags = flags.at[plaquette_idx, first_qubit_idx].set(1)
+        flags = flags.at[plaquette_idx, first_qubit_idx + (length - 1)].set(-1)
+        flags = flags.at[plaquette_idx, first_qubit_idx + length].set(-1)
+        flags = flags.at[plaquette_idx, first_qubit_idx + length + (length - 1)].set(1)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
     for plaquette_idx in range(0, plaquette_count):        
@@ -107,7 +107,7 @@ def single_qubit_terms(length, width, sigma = jnp.array(qt.operators.sigmaz().da
     hilbert_size = 2 ** qubit_count
     flags = jnp.zeros((qubit_count, qubit_count), dtype=jnp.bool_)
     for qubit_idx in range(0, qubit_count):
-        flags[qubit_idx, qubit_idx] = True
+        flags = flags.at[qubit_idx, qubit_idx].set(True)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
     for qubit_idx in range(0, qubit_count):        
@@ -128,31 +128,35 @@ def corner_terms_2(length, width, sigma = jnp.array(qt.operators.sigmaz().data.t
     # width is the number of vertices along the other side
     
     qubit_count = int( length * width )
-    plaquette_count = int( (length - 1) * (width - 1) )
-    corner_count = int( plaquette_count * 4 )
+    edge_count = int( (length - 1) * width + (width - 1) * length )
     hilbert_size = 2 ** qubit_count
-    flags = jnp.zeros((corner_count, qubit_count), dtype=jnp.bool_)
-    corner_idx = 0
-    for plaquette_idx in range(0, plaquette_count):
-        (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length - 1 ) )
-        first_qubit_idx = length * plaquette_row + plaquette_remainder
-        flags[corner_idx, first_qubit_idx] = True
-        flags[corner_idx, first_qubit_idx + 1] = True
-        flags[corner_idx + 1, first_qubit_idx] = True
-        flags[corner_idx + 1, first_qubit_idx + length] = True
-        flags[corner_idx + 2, first_qubit_idx + 1] = True
-        flags[corner_idx + 2, first_qubit_idx + 1 + length] = True
-        flags[corner_idx + 3, first_qubit_idx + length] = True
-        flags[corner_idx + 3, first_qubit_idx + 1 + length] = True
-        corner_idx += 4
+    flags = jnp.zeros((edge_count, qubit_count), dtype=jnp.bool_)
+    edge_idx = 0
+    for qubit_idx in range(0, qubit_count):
+        (qubit_row, qubit_remainder) = onp.divmod(qubit_idx, length)
+        if qubit_remainder == int(length - 1) and qubit_row < int(width - 1):
+            flags = flags.at[edge_idx, qubit_idx].set(True)
+            flags = flags.at[edge_idx, qubit_idx + length].set(True)
+            edge_idx += 1
+        elif qubit_remainder < int(length - 1) and qubit_row < int(width - 1):
+            flags = flags.at[edge_idx, qubit_idx].set(True)
+            flags = flags.at[edge_idx, qubit_idx + 1].set(True)
+            flags = flags.at[edge_idx + 1, qubit_idx].set(True)
+            flags = flags.at[edge_idx + 1, qubit_idx + length].set(True)
+            edge_idx += 2
+        elif qubit_remainder < int(length - 1) and qubit_row == int(width - 1):
+            flags = flags.at[edge_idx, qubit_idx].set(True)
+            flags = flags.at[edge_idx, qubit_idx + 1].set(True)
+            edge_idx += 1
+    print('corner_terms_flags', flags)
     
-    assert(corner_idx == corner_count)
+    assert(edge_idx == edge_count)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
-    for corner_idx in range(0, corner_count):        
+    for edge_idx in range(0, edge_count):        
         matrices = []
         for qubit_idx in range(0, qubit_count):
-            if flags[corner_idx, qubit_idx] == True:
+            if flags[edge_idx, qubit_idx] == True:
                 matrices.append(sigma)
             else:
                 matrices.append(jnp.identity(2,dtype=jnp.complex128) )
@@ -175,10 +179,11 @@ def plaquette_terms_2(length, width,
     for plaquette_idx in range(0, plaquette_count):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length - 1 ) )
         first_qubit_idx = length * plaquette_row + plaquette_remainder
-        flags[plaquette_idx, first_qubit_idx] = 1
-        flags[plaquette_idx, first_qubit_idx + 1] = -1
-        flags[plaquette_idx, first_qubit_idx + length] = -1
-        flags[plaquette_idx, first_qubit_idx + 1 + length] = 1
+        flags = flags.at[plaquette_idx, first_qubit_idx].set(1)
+        flags = flags.at[plaquette_idx, first_qubit_idx + 1].set(-1)
+        flags = flags.at[plaquette_idx, first_qubit_idx + length].set(-1)
+        flags = flags.at[plaquette_idx, first_qubit_idx + 1 + length].set(1)
+    print('plaquette_terms_flags', flags)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
     for plaquette_idx in range(0, plaquette_count):        
@@ -214,7 +219,8 @@ def single_qubit_terms_2(length, width, sigma = jnp.array(qt.operators.sigmaz().
     hilbert_size = 2 ** qubit_count
     flags = jnp.zeros((qubit_count, qubit_count), dtype=jnp.bool_)
     for qubit_idx in range(0, qubit_count):
-        flags[qubit_idx, qubit_idx] = True
+        flags = flags.at[qubit_idx, qubit_idx].set(True)
+    print('single_qubit_flags', flags)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
     for qubit_idx in range(0, qubit_count):        
@@ -250,14 +256,14 @@ def corner_terms_3(length, width, sigma = jnp.array(qt.operators.sigmaz().data.t
     for plaquette_idx in range(0, plaquette_count_1):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length / 2 ) )
         first_qubit_idx = (length * 2) * plaquette_row + 2 * plaquette_remainder
-        flags_1[corner_idx, first_qubit_idx] = True
-        flags_1[corner_idx, first_qubit_idx + 1] = True
-        flags_1[corner_idx + 1, first_qubit_idx] = True
-        flags_1[corner_idx + 1, first_qubit_idx + length] = True
-        flags_1[corner_idx + 2, first_qubit_idx + 1] = True
-        flags_1[corner_idx + 2, first_qubit_idx + 1 + length] = True
-        flags_1[corner_idx + 3, first_qubit_idx + length] = True
-        flags_1[corner_idx + 3, first_qubit_idx + 1 + length] = True
+        flags_1 = flags_1.at[corner_idx, first_qubit_idx].set(True)
+        flags_1 = flags_1.at[corner_idx, first_qubit_idx + 1].set(True)
+        flags_1 = flags_1.at[corner_idx + 1, first_qubit_idx].set(True)
+        flags_1 = flags_1.at[corner_idx + 1, first_qubit_idx + length].set(True)
+        flags_1 = flags_1.at[corner_idx + 2, first_qubit_idx + 1].set(True)
+        flags_1 = flags_1.at[corner_idx + 2, first_qubit_idx + 1 + length].set(True)
+        flags_1 = flags_1.at[corner_idx + 3, first_qubit_idx + length].set(True)
+        flags_1 = flags_1.at[corner_idx + 3, first_qubit_idx + 1 + length].set(True)
         corner_idx += 4
     
     assert(corner_idx == corner_count_1)
@@ -266,14 +272,14 @@ def corner_terms_3(length, width, sigma = jnp.array(qt.operators.sigmaz().data.t
     for plaquette_idx in range(0, plaquette_count_2):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length / 2 - 1 ) )
         first_qubit_idx = length + 1 + (length * 2) * plaquette_row + 2 * plaquette_remainder
-        flags_2[corner_idx, first_qubit_idx] = True
-        flags_2[corner_idx, first_qubit_idx + 1] = True
-        flags_2[corner_idx + 1, first_qubit_idx] = True
-        flags_2[corner_idx + 1, first_qubit_idx + length] = True
-        flags_2[corner_idx + 2, first_qubit_idx + 1] = True
-        flags_2[corner_idx + 2, first_qubit_idx + 1 + length] = True
-        flags_2[corner_idx + 3, first_qubit_idx + length] = True
-        flags_2[corner_idx + 3, first_qubit_idx + 1 + length] = True
+        flags_2 = flags_2.at[corner_idx, first_qubit_idx].set(True)
+        flags_2 = flags_2.at[corner_idx, first_qubit_idx + 1].set(True)
+        flags_2 = flags_2.at[corner_idx + 1, first_qubit_idx].set(True)
+        flags_2 = flags_2.at[corner_idx + 1, first_qubit_idx + length].set(True)
+        flags_2 = flags_2.at[corner_idx + 2, first_qubit_idx + 1].set(True)
+        flags_2 = flags_2.at[corner_idx + 2, first_qubit_idx + 1 + length].set(True)
+        flags_2 = flags_2.at[corner_idx + 3, first_qubit_idx + length].set(True)
+        flags_2 = flags_2.at[corner_idx + 3, first_qubit_idx + 1 + length].set(True)
         corner_idx += 4
     
     assert(corner_idx == corner_count_2)
@@ -320,18 +326,18 @@ def plaquette_terms_3(length, width,
     for plaquette_idx in range(0, plaquette_count_1):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length / 2 ) )
         first_qubit_idx = (length * 2) * plaquette_row + 2 * plaquette_remainder
-        flags_1[plaquette_idx, first_qubit_idx] = 1
-        flags_1[plaquette_idx, first_qubit_idx + 1] = -1
-        flags_1[plaquette_idx, first_qubit_idx + length] = -1
-        flags_1[plaquette_idx, first_qubit_idx + 1 + length] = 1
+        flags_1 = flags_1.at[plaquette_idx, first_qubit_idx].set(1)
+        flags_1 = flags_1.at[plaquette_idx, first_qubit_idx + 1].set(-1)
+        flags_1 = flags_1.at[plaquette_idx, first_qubit_idx + length].set(-1)
+        flags_1 = flags_1.at[plaquette_idx, first_qubit_idx + 1 + length].set(1)
     
     for plaquette_idx in range(0, plaquette_count_2):
         (plaquette_row, plaquette_remainder) = onp.divmod(plaquette_idx, ( length / 2 - 1 ) )
         first_qubit_idx = length + 1 + (length * 2) * plaquette_row + 2 * plaquette_remainder
-        flags_2[plaquette_idx, first_qubit_idx] = 1
-        flags_2[plaquette_idx, first_qubit_idx + 1] = -1
-        flags_2[plaquette_idx, first_qubit_idx + length] = -1
-        flags_2[plaquette_idx, first_qubit_idx + 1 + length] = 1
+        flags_2 = flags_2.at[plaquette_idx, first_qubit_idx].set(1)
+        flags_2 = flags_2.at[plaquette_idx, first_qubit_idx + 1].set(-1)
+        flags_2 = flags_2.at[plaquette_idx, first_qubit_idx + length].set(-1)
+        flags_2 = flags_2.at[plaquette_idx, first_qubit_idx + 1 + length].set(1)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
     for plaquette_idx in range(0, plaquette_count_1):        
@@ -391,7 +397,7 @@ def single_qubit_terms_3(length, width, sigma = jnp.array(qt.operators.sigmaz().
     hilbert_size = 2 ** qubit_count
     flags = jnp.zeros((qubit_count, qubit_count), dtype=jnp.bool_)
     for qubit_idx in range(0, qubit_count):
-        flags[qubit_idx, qubit_idx] = True
+        flags = flags.at[qubit_idx, qubit_idx].set(True)
     
     hamiltonian = jnp.zeros((hilbert_size, hilbert_size), dtype=jnp.complex128)
     for qubit_idx in range(0, qubit_count):        
