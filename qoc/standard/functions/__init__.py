e6d778669c6e1532aa790ce062aa4fa3333d746f
Lamb-b <57016503+Lamb-b@users.noreply.github.com>
2022-08-11 16:31:32 -0500

    Add files via upload

--- qoc/standard/functions/__init__.py
+++ qoc/standard/functions/__init__.py
@@ -13,18 +13,21 @@ from qoc.standard.functions.convenience import (commutator,
 from qoc.standard.functions.generatingHamiltonian import (corner_terms,
                                                 plaquette_terms,
                                                 single_qubit_terms,
+                                                single_qubit_terms_custom,
                                                 corner_terms_2,
                                                 plaquette_terms_2,
                                                 single_qubit_terms_2,
+                                                single_qubit_terms_custom_2,
                                                 corner_terms_3,
                                                 plaquette_terms_3,
-                                                single_qubit_terms_3)
+                                                single_qubit_terms_3,
+                                                single_qubit_terms_custom_3,)
 
 __all__ = [
     "commutator", "conjugate_transpose", "krons", "matmuls",
     "rms_norm",
     "column_vector_list_to_matrix", "matrix_to_column_vector_list",
-    "corner_terms", "plaquette_terms", "single_qubit_terms",
-    "corner_terms_2", "plaquette_terms_2", "single_qubit_terms_2",
-    "corner_terms_3", "plaquette_terms_3", "single_qubit_terms_3"
+    "corner_terms", "plaquette_terms", "single_qubit_terms", "single_qubit_terms_custom",
+    "corner_terms_2", "plaquette_terms_2", "single_qubit_terms_2", "single_qubit_terms_custom_2",
+    "corner_terms_3", "plaquette_terms_3", "single_qubit_terms_3", "single_qubit_terms_custom_3",
 ]
